4d93d90665b0631cb47ec01ab7c544e4fe85c08f
Yatao Li <yatli@microsoft.com>
2019-02-12 17:21:00 +0800

    support both x64 and x86

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,41 +1,23 @@
 ﻿
 $ErrorActionPreference = 'Stop';
 
-
-$packageName= 'cytoscape'
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/cytoscape/cytoscape/releases/download/3.7.1/Cytoscape_3_7_1_windows_64bit.exe'
-
 $packageArgs = @{
-  packageName   = $packageName
-  unzipLocation = $toolsDir
-  fileType      = 'exe'
-  url           = $url
+  packageName    = 'cytoscape'
+  softwareName   = 'Cytoscape*'
 
-  silentArgs   = '-q'
-  validExitCodes= @(0)
+  unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  fileType       = 'exe'
 
-  softwareName  = 'Cytoscape*'
-  checksum      = 'F0232B7234E5A43303E1ECC2D73A7F96'
-  checksumType  = 'md5'
+  url            = 'https://github.com/cytoscape/cytoscape/releases/download/3.7.1/Cytoscape_3_7_1_windows_32bit.exe'
+  url64bit       = 'https://github.com/cytoscape/cytoscape/releases/download/3.7.1/Cytoscape_3_7_1_windows_64bit.exe'
+
+  silentArgs     = '-q'
+  validExitCodes = @(0)
+
+  checksum       = 'C0AA02DA3FD5A30693B716A745B6EE04'
+  checksum64     = 'F0232B7234E5A43303E1ECC2D73A7F96'
+  checksumType   = 'md5'
+  checksumType64 = 'md5'
 }
 
 Install-ChocolateyPackage @packageArgs
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
